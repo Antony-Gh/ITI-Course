0a784d7238f6ff7771a026abea414043c89b8db2
Anthony Gaius <knkmam05@gmail.com>
2026-08-26 11:26:39 +0300

    Initial commit

--- LCD_KPD/APP/main.c
+++ LCD_KPD/APP/main.c
@@ -5,8 +5,8 @@
  *      Author: AnthonyGaius
  */
 
-#include "../LIB/BIT_MATH.h"
 #include "../LIB/STD_TYPES.h"
+#include "../LIB/BIT_MATH.h"
 
 #ifndef F_CPU
 #define F_CPU 8000000UL /* 8 MHz clock speed as default */
@@ -14,152 +14,48 @@
 
 #include <util/delay.h>
 
-#include "../HAL/LCD/HLCD_interface.h"
-#include "../HAL/PB/HPB_interface.h"
-#include "../HAL/SSD/HSSD_interface.h"
 #include "../MCAL/DIO/MDIO_interface.h"
-
-u8 UpperA[8] = {0b00100, 0b01010, 0b10001, 0b10001,
-                0b11111, 0b10001, 0b10001, 0b00000};
-
-u8 LowerA[8] = {0b00000, 0b00000, 0b01110, 0b00001,
-                0b01111, 0b10001, 0b01111, 0b00000};
-
-void DisplaySpecialCharecter(u8 *pattern, u8 block_num, u8 row, u8 col) {
-  HLCD_voidCreateCustomChar(pattern, block_num);
-  HLCD_voidGoToXY(row, col);
-  HLCD_voidSendData(block_num);
-}
-
-void setup(void) {
-  DIO_voidInit();
-
-  // This Initialize the pin (D0) for the units SSD
-  HSSD_voidInit();
-
-  // Initialize the other pin (D1) for the tens SSD
-  DIO_enumSetPinDirection(DIO_PORTD, DIO_PIN1, DIO_OUTPUT);
-
-  // Configure Port B Pin 6 (Mode) and Pin 7 (Increase) as Push Buttons
-  HPB_voidInit(DIO_PORTB, DIO_PIN6, HPB_PULL_DOWN);
-  HPB_voidInit(DIO_PORTB, DIO_PIN7, HPB_PULL_DOWN);
-
-  HLCD_voidInit();
-
-  HLCD_voidClearScreen();
-
-  // My Name on the LCD In Sinusoidal Form
-  u8 name[] = "ANTHONY GAIUS";
-  for (u8 i = 0; i < 13; i++) {
-    if (i % 2 == 0) {
-      HLCD_voidGoToXY(0, i);
-    } else {
-      HLCD_voidGoToXY(1, i);
-    }
-    HLCD_voidSendData(name[i]);
-  }
-
-  _delay_ms(3000);
-
-  HLCD_voidClearScreen();
-
-  DisplaySpecialCharecter(UpperA, 0, 0, 5);
-  DisplaySpecialCharecter(LowerA, 1, 1, 5);
-
-  _delay_ms(3000);
-
-  HLCD_voidClearScreen();
-}
-
-void loop(void) {
-  static u8 counter = 0;
-  static u8 mode = 0;
-  static u8 tick = 0;
-  
-  static u8 last_mode_state = 0;
-  static u8 last_action_state = 0;
-  
-  u8 current_mode_state = 0;
-  u8 current_action_state = 0;
-  HPB_voidGetState(DIO_PORTB, DIO_PIN6, HPB_PULL_DOWN, &current_mode_state);
-  HPB_voidGetState(DIO_PORTB, DIO_PIN7, HPB_PULL_DOWN, &current_action_state);
-  
-  // Mode Button Logic (Rising Edge)
-  if (current_mode_state == HPB_PRESSED && last_mode_state == HPB_RELEASED) {
-
-	  // Change the mode
-      mode = !mode;
-
-      // We reset the counter and ticks on the mode change
-      tick = 0;
-      counter = 0;
-  }
-  
-  last_mode_state = current_mode_state;
-  
-  if (mode == 1) {
-      // Manual Mode = Increase on button press (Rising Edge)
-      if (current_action_state == HPB_PRESSED && last_action_state == HPB_RELEASED) {
-          counter++;
-          if (counter > 99) counter = 0;
-      }
-  } else {
-      // Auto Mode = Increase every 1 second (25 ticks of 40ms = 1000ms)
-      tick++;
-      if (tick >= 25) {
-          tick = 0;
-          counter++;
-          if (counter > 99) counter = 0;
-      }
-  }
-  last_action_state = current_action_state;
-
-  // Maximum value for the u8 type
-  static u8 last_disp_counter = 255;
-  static u8 last_disp_mode = 255;
-
-  if (mode != last_disp_mode) {
-      HLCD_voidGoToXY(0, 1);
-      if (mode == 0) {
-          HLCD_voidSendString("Mode: Auto    ");
-      } else {
-          HLCD_voidSendString("Mode: Manual  ");
-      }
-      last_disp_mode = mode;
-  }
-  
-  if (counter != last_disp_counter) {
-      HLCD_voidGoToXY(1, 1);
-      HLCD_voidSendString("Counter: ");
-      HLCD_voidSendData((counter / 10) + '0');
-      HLCD_voidSendData((counter % 10) + '0');
-      HLCD_voidSendString("   ");
-      last_disp_counter = counter;
-  }
-
-  // Multiplexing Display (takes 40ms total)
-  u8 tens = counter / 10;
-  u8 units = counter % 10;
-
-  // Display Tens
-  DIO_enumSetPinValue(DIO_PORTD, DIO_PIN0, DIO_LOW);
-  HSSD_voidDisplayNumber(tens);
-  DIO_enumSetPinValue(DIO_PORTD, DIO_PIN1, DIO_HIGH);
-  _delay_ms(20);
-
-  // Display Units
-  DIO_enumSetPinValue(DIO_PORTD, DIO_PIN1, DIO_LOW);
-  HSSD_voidDisplayNumber(units);
-  DIO_enumSetPinValue(DIO_PORTD, DIO_PIN0, DIO_HIGH);
-  _delay_ms(20);
-}
+#include "../HAL/LCD/HLCD_interface.h"
+#include "../HAL/KPD/KPD_interface.h"
 
 int main(void) {
-  setup();
+    /* Initialize DIO, LCD, and KPD */
+    DIO_voidInit();
+    HLCD_voidInit();
+    KPD_voidInit();
 
-  while (1) {
-    loop();
-  }
+    u8 local_u8CharCount = 0;
+    u8 local_u8Key;
 
-  return 0;
+    /* Initial state */
+    HLCD_voidClearScreen();
+    HLCD_voidGoToXY(0, 0);
+
+    while (1) {
+        local_u8Key = KPD_u8GetPressedKey();
+        
+        if (local_u8Key != KPD_NOT_PRESSED) {
+            /* If screen is full, clear it and reset counter */
+            if (local_u8CharCount == 32) {
+                HLCD_voidClearScreen();
+                local_u8CharCount = 0;
+                /* HLCD_voidClearScreen already resets cursor to 0,0 typically, 
+                 * but we can explicitly set it just to be safe */
+                HLCD_voidGoToXY(0, 0);
+            }
+            
+            /* If we reached the end of the first line, move to the second line */
+            if (local_u8CharCount == 16) {
+                HLCD_voidGoToXY(1, 0);
+            }
+            
+            /* Print the character on the LCD */
+            HLCD_voidSendData(local_u8Key);
+            
+            /* Increment character count */
+            local_u8CharCount++;
+        }
+    }
+
+    return 0;
 }
---
+++ LCD_KPD/HAL/KPD/KPD_program.c
@@ -0,0 +1,72 @@
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+#include "../../MCAL/DIO/MDIO_interface.h"
+#include "KPD_interface.h"
+#include "KPD_config.h"
+#include "KPD_private.h"
+#include <util/delay.h>
+
+void KPD_voidInit(void) {
+    /* Set Rows as Output */
+    DIO_enumSetPinDirection(KPD_PORT, KPD_R0_PIN, DIO_OUTPUT);
+    DIO_enumSetPinDirection(KPD_PORT, KPD_R1_PIN, DIO_OUTPUT);
+    DIO_enumSetPinDirection(KPD_PORT, KPD_R2_PIN, DIO_OUTPUT);
+    DIO_enumSetPinDirection(KPD_PORT, KPD_R3_PIN, DIO_OUTPUT);
+
+    /* Set Columns as Input */
+    DIO_enumSetPinDirection(KPD_PORT, KPD_C0_PIN, DIO_INPUT);
+    DIO_enumSetPinDirection(KPD_PORT, KPD_C1_PIN, DIO_INPUT);
+    DIO_enumSetPinDirection(KPD_PORT, KPD_C2_PIN, DIO_INPUT);
+    DIO_enumSetPinDirection(KPD_PORT, KPD_C3_PIN, DIO_INPUT);
+
+    /* Initialize Rows to HIGH (inactive) */
+    DIO_enumSetPinValue(KPD_PORT, KPD_R0_PIN, DIO_HIGH);
+    DIO_enumSetPinValue(KPD_PORT, KPD_R1_PIN, DIO_HIGH);
+    DIO_enumSetPinValue(KPD_PORT, KPD_R2_PIN, DIO_HIGH);
+    DIO_enumSetPinValue(KPD_PORT, KPD_R3_PIN, DIO_HIGH);
+
+    /* Initialize Columns to HIGH (Enable Internal Pull-up Resistors) */
+    DIO_enumSetPinValue(KPD_PORT, KPD_C0_PIN, DIO_HIGH);
+    DIO_enumSetPinValue(KPD_PORT, KPD_C1_PIN, DIO_HIGH);
+    DIO_enumSetPinValue(KPD_PORT, KPD_C2_PIN, DIO_HIGH);
+    DIO_enumSetPinValue(KPD_PORT, KPD_C3_PIN, DIO_HIGH);
+}
+
+u8 KPD_u8GetPressedKey(void) {
+    u8 Local_u8PressedKey = KPD_NOT_PRESSED;
+    u8 Local_u8RowIdx, Local_u8ColIdx, Local_u8PinState;
+    static u8 Local_u8KPDArr[4][4] = KPD_KEYS;
+    static u8 Local_u8RowArr[4] = {KPD_R0_PIN, KPD_R1_PIN, KPD_R2_PIN, KPD_R3_PIN};
+    static u8 Local_u8ColArr[4] = {KPD_C0_PIN, KPD_C1_PIN, KPD_C2_PIN, KPD_C3_PIN};
+
+    for (Local_u8RowIdx = 0; Local_u8RowIdx < 4; Local_u8RowIdx++) {
+        /* Activate current row (Active LOW) */
+        DIO_enumSetPinValue(KPD_PORT, Local_u8RowArr[Local_u8RowIdx], DIO_LOW);
+
+        /* Read columns */
+        for (Local_u8ColIdx = 0; Local_u8ColIdx < 4; Local_u8ColIdx++) {
+            DIO_enumGetPinValue(KPD_PORT, Local_u8ColArr[Local_u8ColIdx], &Local_u8PinState);
+            
+            if (Local_u8PinState == DIO_LOW) {
+                Local_u8PressedKey = Local_u8KPDArr[Local_u8RowIdx][Local_u8ColIdx];
+
+                /* Wait until key is released (polling) to avoid multiple reads for single press */
+                while (Local_u8PinState == DIO_LOW) {
+                    DIO_enumGetPinValue(KPD_PORT, Local_u8ColArr[Local_u8ColIdx], &Local_u8PinState);
+                }
+                
+                /* Debounce delay */
+                _delay_ms(20);
+                
+                /* Deactivate current row before returning */
+                DIO_enumSetPinValue(KPD_PORT, Local_u8RowArr[Local_u8RowIdx], DIO_HIGH);
+                return Local_u8PressedKey;
+            }
+        }
+
+        /* Deactivate current row (HIGH) */
+        DIO_enumSetPinValue(KPD_PORT, Local_u8RowArr[Local_u8RowIdx], DIO_HIGH);
+    }
+
+    return Local_u8PressedKey;
+}
